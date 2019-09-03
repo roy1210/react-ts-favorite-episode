f879de49190daf36c1268d992f0620b820b7f18f
roy1210 <rswkure@gmail.com>
2019-09-04 07:02:44 +0800

    Counting chosen favorites QTY

--- src/App.tsx
+++ src/App.tsx
@@ -45,8 +45,13 @@ export default function App(): JSX.Element {
   return (
     <React.Fragment>
       <header className='header'>
-        <h1>Rick and Morty</h1>
-        <p>Pick your favorite episode!</p>
+        <div>
+          <h1>Rick and Morty</h1>
+          <p>Pick your favorite episode!</p>
+        </div>
+        <div>
+          <p>Favorite(s): {state.favorites.length}</p>
+        </div>
       </header>
       <section className='episode-layout'>
         {state.episodes.map((episode: IEpisode) => {
