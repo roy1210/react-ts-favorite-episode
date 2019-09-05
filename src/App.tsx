c0b5d27763aacee35d21a380a64d616d02368a5d
roy1210 <rswkure@gmail.com>
2019-09-05 08:23:48 +0800

    Applied react.suspence and lazy to show 'loading...' when fetching from API

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Store } from './Store';
 import { IEpisode, IAction } from './interfaces';
 
+const EpisodeList = React.lazy<any>(() => import('./EpisodesList'));
+
 export default function App(): JSX.Element {
   const { state, dispatch } = React.useContext(Store);
 
@@ -41,6 +43,12 @@ export default function App(): JSX.Element {
     return dispatch(dispatchObj);
   };
 
+  const props = {
+    episodes: state.episodes,
+    toggleFavAction,
+    favorites: state.favorites
+  };
+
   const zeroOrOneFavorite = <p>Favorite: {state.favorites.length}</p>;
 
   const moreThanOneFavorites = <p>Favorites: {state.favorites.length}</p>;
@@ -59,28 +67,11 @@ export default function App(): JSX.Element {
             : moreThanOneFavorites}
         </div>
       </header>
-      <section className='episode-layout'>
-        {state.episodes.map((episode: IEpisode) => {
-          return (
-            <section key={episode.id} className='episode-box'>
-              <img
-                src={episode.image.medium}
-                alt={`Rick and Mort ${episode.name}`}
-              />
-              <div>{episode.name}</div>
-              <section>
-                <div>
-                  Season: {episode.season} Number: {episode.number}
-                </div>
-                <button type='button' onClick={() => toggleFavAction(episode)}>
-                  {state.favorites.includes(episode) ? 'Remove' : 'Favorite'}
-                  {/* {state.favorites.find((fav:IEpisode) => fav.id === episode.id)? "Unfav": 'Fav'} */}
-                </button>
-              </section>
-            </section>
-          );
-        })}
-      </section>
+      <React.Suspense fallback={<div>loading...</div>}>
+        <section className='episode-layout'>
+          <EpisodeList {...props} />
+        </section>
+      </React.Suspense>
     </React.Fragment>
   );
 }
---
+++ src/EpisodesList.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { IEpisode } from './interfaces';
+
+export default function EpisodesList(props: any): Array<JSX.Element> {
+  const { episodes, toggleFavAction, favorites } = props;
+  return episodes.map((episode: IEpisode) => {
+    return (
+      <section key={episode.id} className='episode-box'>
+        <img src={episode.image.medium} alt={`Rick and Mort ${episode.name}`} />
+        <div>{episode.name}</div>
+        <section style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div>
+            Season: {episode.season} Number: {episode.number}
+          </div>
+          <button type='button' onClick={() => toggleFavAction(episode)}>
+            {favorites.includes(episode) ? 'Remove' : 'Favorite'}
+            {/* {favorites.find((fav: IEpisode) => fav.id === episode.id)
+              ? 'Unfav'
+              : 'Fav'} */}
+          </button>
+        </section>
+      </section>
+    );
+  });
+}
